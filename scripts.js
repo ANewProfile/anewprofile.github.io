b50a990e8aa86efd7a47211a9217c4d9fdd69f38
Unofficial Cuber <95180963+ANewProfile@users.noreply.github.com>
2024-10-27 06:06:15 -0400

    Stucf

--- scripts.js
+++ scripts.js
@@ -10,32 +10,22 @@ $(document).ready(function() {
         }
         else {
             $("#list"+which_list).empty();
-            // console.log("emptied list "+which_list)
         }
     }
 
     function clear_lists(start_list) {
-        // console.log("start_list: "+start_list);
         var next_list = start_list+1;
-        // console.log("next_list: "+next_list);
-        // console.log($("#list"+6));
         if ($("#list"+next_list).length > 0) {
-            // console.log("clearing #list"+start_list);
             clear_list(start_list, true);
-            // console.log("cleared list "+start_list+" and added option");
-            // console.log("next list: "+next_list);
             clear_lists(next_list);
         } else {
             clear_list(start_list, false);
-            // console.log("cleared list "+start_list+" and didn't add option");
         }
     }
 
     function set_options_list(which_list, items) {
-        // clear_lists(which_list);
         _.map(items, function(item) {
   
-            // var list_item = document.createElement("li");
             var option = document.createElement("option");
             $(option).addClass("item-button"+which_list).html(item[0]);
             $(option).data("items", item[1]);
@@ -45,13 +35,11 @@ $(document).ready(function() {
                 set_list_generic(which_list+1, $(selectedOption).data("items"));
         });
   
-        // $(list_item).html($(button))
         $("#list"+which_list).append(option);
       });
     }
 
     function set_li_list(which_list, items) {
-        // $("#list"+which_list).empty();
         console.log(items);
         _.map(items, function (item) {
             var element = document.createElement("li");
@@ -66,8 +54,8 @@ $(document).ready(function() {
             $("#list"+which_list).append(description_elm);
             $("#list"+which_list).append("<br>");
         });
+        $("#last").css("color", "black");
         $("#list"+which_list).append(document.createElement("br"));
-        $("#list"+which_list).append("<a href='https://clover.caltech.edu/aav-repository' id='aav-repo-link'>See how others have used these capsids</a>");
     }
 
     function set_list_generic(which_list, items) {
@@ -76,11 +64,9 @@ $(document).ready(function() {
         }
         clear_lists(which_list);
         if (Array.isArray(items[0])) {
-            // console.log("setting list "+which_list);
             set_options_list(which_list, items);
         }
         else {
-            // console.log("setting final list");
             set_li_list(which_list, items);
         }
     }
@@ -91,23 +77,18 @@ $(document).ready(function() {
             if (text.toLowerCase().includes(capsid.toLowerCase())) {
                 capsid_descriptions[capsid].forEach(function (description) {
                     var new_link = document.createElement("a");
-                    // console.log($(new_link));
-                    // if ($(new_link)) {
-                    //     console.log("exists")
-                    // }
                     new_link.setAttribute("href", description[1]);
                     $(new_link).html(description[0]);
                     descriptions.push(new_link);
-                    // descriptions.push(description);
                 });
             }
         });
-        // console.log(descriptions)
         return descriptions;
     }
 
     $("#clear").on("click", function() {
         $(".item-button1").css("background-color", "white");
+        $("#last").css("color", "white");
         var first_list = 1;
         clear_lists(first_list);
         set_list_generic(first_list, capsid_selections)
@@ -115,26 +96,4 @@ $(document).ready(function() {
     });
 
     set_list_generic(1, capsid_selections);
-    // console.log($("#list1"));
-  
-  });
-
-/*
-
-dictionary:
-capsid_descriptions = {
-  "name of capsid": "description",
-  "next capsid": "next description",
-}
-
-*/
-
-/*
-
-a = []
-for capsid in capsid_descriptions.keys():
-  if text.lower().contains(capsid.lower()):
-    a.append(capsid_descriptions[capsid])
-return " ".join(a)
-
-*/
+});
